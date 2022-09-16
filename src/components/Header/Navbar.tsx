fa14fac6c98a9a810b4d59e091240d2ac4d669b7
Alimuhammadf5 <alimuhammadfc5@gmail.com>
2022-09-15 23:26:03 -0700

    navbar

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,7 +1,29 @@
 import { FC } from "react";
+import Router from "next/router";
 
 const Navbar: FC = () => {
-	return <nav>Navbar</nav>;
+	return (
+		<nav className="px-6 w-screen fixed z-40 top-0 bg-bg h-20 flex justify-center items-center">
+			<section className="max-w-screen-xl w-full flex justify-between items-center space-x-10">
+				<h1
+					onClick={() => Router.push("/")}
+					className="font-bold text-2xl cursor-pointer"
+				>
+					Clothing.
+				</h1>
+				<div className=" col-span-1 flex justify-end items-center">
+					{/* <ShoppingCart /> */}
+					{/* <User /> */}
+					<button className="hidden bg-bg text-blue-600 drop-shadow-md h-12 w-32 rounded-2xl hover:bg-gray-100 transition-colors duration-300 font-medium">
+						Sign in
+					</button>
+					<button className="hidden bg-blue-600 text-white  h-12 w-32 rounded-2xl hover:bg-blue-700 transition-colors duration-300 ">
+						Sign up
+					</button>
+				</div>
+			</section>
+		</nav>
+	);
 };
 
 export default Navbar;
---
+++ src/recoil/cartState.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+const cartState = atom({
+	key: "cartState",
+	default: false,
+});
